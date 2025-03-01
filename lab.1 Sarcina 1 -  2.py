4d661d52f0b2bb6ead5c18b2b9b6b21d13ea072b
GicuGheorghe <strut.gicu@mail.ru>
2025-03-01 13:07:43 +0200

    Add files via upload

---
+++ lab.1 Sarcina 1 -  2.py
@@ -0,0 +1,55 @@
+# Sarcina 1
+
+name = input("Introduceți numele dvs.: ")
+print(f"Salut, {name}, Bine ai venit!" )
+
+
+real_value = 3.14
+short_text = "Laborator"
+a = short_text
+short_text = "kkokok"
+print(a)
+integer_value = 42
+long_text = """Python este un limbaj de programare
+ foarte popular și ușor de învățat.
+ Se folosește în multe domenii, inclusiv AI
+ și dezvoltare web."""
+
+print(type(real_value))
+print(type(integer_value))
+
+print("Lungimea variabilei short_text:", len(short_text))
+
+print("Textul transformat in litere mari:", long_text.upper())
+
+substring = short_text[0:4]
+print("Subsir extras din short_text:", substring)
+
+print("Mesaj format cu metoda string_format : {} {} " .format(integer_value,real_value))
+print(f"Mesaj format cu metoda f-string,Variabila real_value are valoarea {real_value}" )
+
+
+# Sarcina 2
+
+#a)
+txt = "More results from text..."
+substr = txt[4:12]
+print(substr)
+print(substr.strip()) 
+# printeaza " results" si "results" ,.strip elimina spatiile goale
+
+#b)
+txt = "More results from text..."
+print(txt.split())
+# desparte propozitia pe cuvinte
+
+
+#c)
+age = 36
+txt = "My name is Mary, and I am {}"
+print(txt.format(age))
+# atribuie variabilei age valoare in textul "My name is Mary, and I am {}" inlocuind valoarea 36
+
+
+
+
